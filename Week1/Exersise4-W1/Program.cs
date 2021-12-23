0194ac188bf8369dd35bc775bd0fbee2fffeafa5
Your Name <ntri1104@gmail.com>
2021-12-23 09:35:32 +0700

    add ex4

---
+++ Week1/Exersise4-W1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exersise4_W1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius of circle: ");
+            double RadiusOfCircle = Convert.ToDouble(Console.ReadLine());
+            double PerimeterOfCircle = RadiusOfCircle * 2 * Math.PI;
+            Console.WriteLine($"Perimeter of Circle: {PerimeterOfCircle}");
+        }
+    }
+}
