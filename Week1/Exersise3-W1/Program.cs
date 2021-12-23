52cbfda971023c541de5913781c13ab6f7abad2a
Your Name <ntri1104@gmail.com>
2021-12-23 09:33:18 +0700

    add ex3

---
+++ Week1/Exersise3-W1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exersise3_W1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int firstnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Operation: ");
+            string? operation = Console.ReadLine();
+            Console.WriteLine("Enter second number: ");
+            int secondnumber = Convert.ToInt32(Console.ReadLine());;
+            int minus = firstnumber - secondnumber;
+            int plus = firstnumber + secondnumber;
+            int divide = firstnumber / secondnumber;
+            int multiple = firstnumber * secondnumber;
+            if (operation == "+" || operation == "-" || operation == "x" || operation == "/")
+            {
+                if (operation == "+") 
+                {
+                    Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {plus}");
+                }
+                if (operation == "-") 
+                {
+                    Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {minus}");
+                }
+                if (operation == "x") 
+                {
+                    Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {multiple}");
+                }
+                if (operation == "/") 
+                {
+                    Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {divide}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation");
+            }
+        }
+    }
+}
