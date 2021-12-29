0b726453bb392b3b9ab31c76addbfa6452f729b3
Your Name <ntri1104@gmail.com>
2021-12-29 21:16:25 +0700

    rebutal exersise 3

--- Week1/Exersise3-W1/Program.cs
+++ Week1/Exersise3-W1/Program.cs
@@ -6,45 +6,15 @@ namespace Exersise3_W1
     {
         static void Main(string[] args)
         {
-            
-                Console.WriteLine("Enter first number: ");
-                int firstnumber = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Operation: ");
-                string? operation = Console.ReadLine();
-                Console.WriteLine("Enter second number: ");
-                int secondnumber = Convert.ToInt32(Console.ReadLine());
-            do 
-            {
-                int result;
-            
-            switch (operation)
-                {
-                    case "+":
-                        result = firstnumber + secondnumber;
-                        Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
-                        break;
-                    case "-":
-                        result = firstnumber - secondnumber;
-                        Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
-                        break;
-                    case "x":
-                        result = firstnumber * secondnumber;
-                        Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
-                        break;
-                    case "/":
-                        result = firstnumber / secondnumber;
-                        Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid operation, please type the correct operation.");
-                        operation = "\0";
-                        break;
-                }
-            } while (operation == "\0");
-        }
-    }
-}
-/*
+            Console.WriteLine("Enter first number: ");
+            int firstnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Operation: ");
+            string? operation = Console.ReadLine();
+            Console.WriteLine("Enter second number: ");
+            int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+
             if (operation == "+") 
             {
                 result = firstnumber + secondnumber;
@@ -68,4 +38,36 @@ namespace Exersise3_W1
             else 
             {
                 Console.WriteLine("Invalid Operation");
-            } */
+            } 
+        }
+    }
+}
+
+// do 
+            // {
+            //     int result;
+            
+            // switch (operation)
+            //     {
+            //         case "+":
+            //             result = firstnumber + secondnumber;
+            //             Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
+            //             break;
+            //         case "-":
+            //             result = firstnumber - secondnumber;
+            //             Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
+            //             break;
+            //         case "x":
+            //             result = firstnumber * secondnumber;
+            //             Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
+            //             break;
+            //         case "/":
+            //             result = firstnumber / secondnumber;
+            //             Console.WriteLine($"{firstnumber} {operation} {secondnumber} = {result}");
+            //             break;
+            //         default:
+            //             Console.WriteLine("Invalid operation, please type the correct operation.");
+            //             operation = "\0";
+            //             break;
+            //     }
+            // } while (operation == "\0");
