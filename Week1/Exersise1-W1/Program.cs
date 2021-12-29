ab79727c394302ae17f840fef978e7ad26ea0ef6
Your Name <ntri1104@gmail.com>
2021-12-29 09:20:12 +0700

    rebuttal

--- Week1/Exersise1-W1/Program.cs
+++ Week1/Exersise1-W1/Program.cs
@@ -10,32 +10,34 @@ namespace Exersise1_W1
             int day30 = 30;
             int day29 = 29;
             int day28 = 28;
+
             Console.WriteLine("Enter year: ");
             int year = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter month: ");
             int month = Convert.ToInt32(Console.ReadLine());
-            if (month > 12)
+            
+            if (month > 0 && month <= 12)
             {
-                Console.WriteLine("Invalid month");
-            }
-            else
-            {
-                if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-                {
-                    Console.WriteLine($"This month has {day31} days");
-                }
-                else if (month == 4 || month == 6 || month == 9 || month == 11) 
-                {
-                    Console.WriteLine($"This month has {day30} days");
-                }
-                else if (month == 2 && year % 4 == 0)
+               if (month == 2 && year % 4 == 0)
                 {
                     Console.WriteLine($"This month has {day29} days");
                 }
+                else if (month == 2 && year % 4 != 0) 
+                {
+                    Console.WriteLine($"This month has {day28} days");
+                }
+                else if (month == 4 || month == 6 || month == 9 || month == 11)
+                {
+                    Console.WriteLine($"This month has {day30} days");
+                }
                 else 
                 {
-                    Console.WriteLine(day28);
+                    Console.WriteLine($"This month has {day31} days");
                 }
+            }
+            else
+            {
+                 Console.WriteLine("Invalid month");
             }
         }
     }
