079197d6f5c3df79569939c81a49565a93a209d6
Tri Nguyen <ntri1104@gmail.com>
2022-03-24 15:45:29 +0700

    create the constructor

--- Week3/Exersise7-W3/Employee.cs
+++ Week3/Exersise7-W3/Employee.cs
@@ -42,5 +42,29 @@ namespace Exersise7_W3
         public int BasicSalary { get; set; }
         public int Salary { get; set; }
         public string? FullName { get; set; }
+
+        public Employee()
+        {
+        
+        }
+        
+        public Employee(int id) 
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Employee(int id, string? firstName, string? lastName, string? sex, int birthDay, int birthMonth, int birthYear, int level, int basicSalary) 
+            : this(id)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Sex = sex;
+            this.BirthDay = birthDay;
+            this.BirthMonth = birthMonth;
+            this.BirthYear = birthYear;
+            this.Level = level;
+            this.BasicSalary = basicSalary;
+        }
     }
 }
